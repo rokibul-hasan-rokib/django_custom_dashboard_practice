527a56e0519654c5e87b06acda0ea9ab78f3abaa
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-14 12:54:33 +0600

    customer model done

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    phone = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
