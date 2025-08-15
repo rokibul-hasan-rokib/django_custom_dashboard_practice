235f01af78da2b267c22ca7e68740f0c0225d984
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-15 13:59:17 +0600

    feat: forms added here in customers

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomerForm
 
-# Create your views here.
+
+def add_customer(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+    else:
+        form = CustomerForm()
+    
+    return render(request, 'customer/add_customer.html', {'form': form})
