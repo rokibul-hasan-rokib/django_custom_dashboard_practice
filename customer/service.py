63b5a261ce626410574c9261f4abac6cf9d959b6
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-08-15 16:41:39 +0600

    feat: service class crud added

--- customer/service.py
+++ customer/service.py
@@ -1,4 +1,5 @@
 from .models import Customer
+from django.shortcuts import get_object_or_404
 
 
 def get_dashboard_data():
@@ -7,3 +8,26 @@ def get_dashboard_data():
         'recent_customers': Customer.objects.order_by('-created_at')[:5],
         'customer_data': Customer.objects.all(),
     }
+    
+def list_customers():
+    return Customer.objects.all()
+
+def get_customer(customer_id: int):
+    return get_object_or_404(Customer, id=customer_id)
+
+
+def create_customer(data: dict):
+    return Customer.objects.create(**data)
+
+
+def update_customer(customer_id: int, data: dict):
+    customer = get_object_or_404(Customer, id=customer_id)
+    for field, value in data.items():
+        setattr(customer, field, value)
+    customer.save()
+    return customer
+
+def delete_customer(customer_id: int):
+    customer = get_customer(customer_id)
+    customer.delete()
+    return True
